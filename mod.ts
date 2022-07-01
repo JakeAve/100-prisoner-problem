b44964f3c850d205eb7b6e6522debed3c32ce946
JakeAve <jacobjavery@gmail.com>
2022-07-01 13:54:33 -0600

    Readability changes

--- mod.ts
+++ mod.ts
@@ -14,44 +14,49 @@ export interface Drawer {
 }
 
 export const createDrawers = (num: number) => {
-  const slips = makeNumArray(num);
+  const prisoners = makeNumArray(num);
 
   const drawers: Drawer[] = [];
   for (let i = 1; i <= num; i++) {
-    const idx = getRandomIndex(slips);
-    const slip = slips[idx];
-    drawers.push({ drawerNumber: i, prisonerNumber: slip });
-    slips.splice(idx, 1);
+    const idx = getRandomIndex(prisoners);
+    const prisonerNumber = prisoners[idx];
+    drawers.push({ drawerNumber: i, prisonerNumber });
+    prisoners.splice(idx, 1);
   }
+
   return drawers;
 };
 
 export const runSimulation = (
   strategy: (drawers: readonly Drawer[], prisonerNumber: number) => Drawer[],
   numberOfSimulations = 1,
-  numberOfDrawers = 100
+  numberOfPrisoners = 100
 ) => {
   let fails = 0;
   for (let i = 0; i < numberOfSimulations; i++) {
-    const drawers = createDrawers(numberOfDrawers);
+    const drawers = createDrawers(numberOfPrisoners);
     Object.freeze(drawers);
-    for (let j = 1; j <= numberOfDrawers; j++) {
-      // As soon as a prisoner fails, the simulation is over
+    for (let j = 1; j <= numberOfPrisoners; j++) {
       const halfTheDrawers = strategy(drawers, j);
+
       if (!Array.isArray(halfTheDrawers))
         throw new Error("Strategy must return an array");
-      const didOpenLessThanHalf = halfTheDrawers.length <= numberOfDrawers / 2;
-      const areValidDrawers = halfTheDrawers.every((b) => drawers.includes(b));
-      const isCorrectNumber = halfTheDrawers.some(
-        (b) => b.prisonerNumber === j
-      );
 
+      const didOpenLessThanHalf =
+        halfTheDrawers.length <= numberOfPrisoners / 2;
       if (!didOpenLessThanHalf)
         throw new Error(`Prisoner ${j} opened more than half the drawers`);
+
+      const areValidDrawers = halfTheDrawers.every((b) => drawers.includes(b));
       if (!areValidDrawers)
         throw new Error(
           `Prisoner ${j} opened invalid drawers ${halfTheDrawers}`
         );
+
+      // if one prisoner fails, the whole simulation fails
+      const isCorrectNumber = halfTheDrawers.some(
+        (b) => b.prisonerNumber === j
+      );
       if (!isCorrectNumber) {
         fails++;
         break;
