dfc59fa0ebbe1c394c0d3e37dd68e9c87f3b1318
JakeAve <jacobjavery@gmail.com>
2022-07-01 13:46:23 -0600

    Added alias in deno args

--- simulate.ts
+++ simulate.ts
@@ -1,14 +1,16 @@
 import { parse } from "https://deno.land/std@0.146.0/flags/mod.ts";
 import { runSimulation } from "./mod.ts";
 
-const parsedArgs = parse(Deno.args);
+const parsedArgs = parse(Deno.args, {
+  alias: { i: "iterations", p: "prisoners" },
+});
 
 const {
   _: [strategyName],
 } = parsedArgs;
 
-const iterations = parsedArgs.iterations ?? parsedArgs.i ?? 1;
-const prisoners = parsedArgs.prisoners ?? parsedArgs.p ?? 100;
+const iterations = parsedArgs.iterations ?? 1;
+const prisoners = parsedArgs.prisoners ?? 100;
 
 const strategy = await import(`./strategies/${strategyName}`);
 
